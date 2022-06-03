55c2148af3ab5c793f6736e2d8dd1e1f26884643
Oliver Nordbjerg <hi@notbjerg.me>
2022-06-03 07:15:32 +0200

    fix: hit map collection
    This marks some of the statements as covered, however:
    
    - Branches are not currently marked
    - It does not seem to be entirely accurate currently

--- cli/src/cmd/forge/coverage.rs
+++ cli/src/cmd/forge/coverage.rs
@@ -11,7 +11,7 @@ use cast::trace::identifier::TraceIdentifier;
 use clap::{AppSettings, ArgEnum, Parser};
 use ethers::{
     abi::Address,
-    prelude::{artifacts, Artifact, Project, ProjectCompileOutput},
+    prelude::{Artifact, Project, ProjectCompileOutput},
     solc::{
         artifacts::contract::CompactContractBytecode,
         sourcemap::{self, SourceMap},
@@ -231,11 +231,7 @@ impl CoverageArgs {
                                 source_map,
                                 hit_map.clone(),
                             );
-                        } else {
-                            println!("Found no hit map: {:?}", artifact_id);
                         }
-                    } else {
-                        println!("Found no source map: {:?}", artifact_id);
                     }
                 }
             }
